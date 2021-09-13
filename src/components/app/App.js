23ba4c005f7da2d0c04c04a6dc833ce8e5029799
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-13 15:03:52 +0200

    set up basic file structure

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "../header/Header.js";
+
+function App() {
+  console.log("test");
+  const name = "neue fische";
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
