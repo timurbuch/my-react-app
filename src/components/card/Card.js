7129d1cf2ce8ef5bf1b47ef7d463a9c81d66e067
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-15 00:57:49 +0200

    Add alt tag to crad image

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,13 +4,15 @@ function Card(props) {
     <div className="card">
       <h2>{props.name}</h2>
       <section className="flex-wrapper">
-        <img src={props.img} />
-        <ul>
-          <li>{props.gender}</li>
-          <li>{props.dateOfBirth}</li>
-          <li>{props.house}</li>
+        <img src={props.img} alt={props.name} />
+        <div className="text-wrapper">
+          <ul>
+            <li>{props.gender}</li>
+            <li>{props.dateOfBirth}</li>
+            <li>{props.house}</li>
+          </ul>
           <button>Show more</button>
-        </ul>
+        </div>
       </section>
     </div>
   );
