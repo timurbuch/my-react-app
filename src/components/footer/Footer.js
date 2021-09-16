341cc49eea5c63ccc131690653c1e05ce3a00b90
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-16 11:06:41 +0200

    Add different backgroundcolors for Navlinks

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -19,25 +19,33 @@ function Footer() {
       <NavLink
         src={gIcon}
         ariaLabel="Gryffindor"
-        className={buttonState === "Gryffindor" ? "FooterButtonActive" : ""}
+        className={
+          buttonState === "Gryffindor" ? "FooterButtonActive--Gryffindor" : ""
+        }
         onClick={() => buttonStateHandler("Gryffindor")}
       />
       <NavLink
         src={hIcon}
         ariaLabel="Hufflepuff"
-        className={buttonState === "Hufflepuff" ? "FooterButtonActive" : ""}
+        className={
+          buttonState === "Hufflepuff" ? "FooterButtonActive--Hufflepuff" : ""
+        }
         onClick={() => buttonStateHandler("Hufflepuff")}
       />
       <NavLink
         src={rIcon}
         ariaLabel="Ravenclaw"
-        className={buttonState === "Ravenclaw" ? "FooterButtonActive" : ""}
+        className={
+          buttonState === "Ravenclaw" ? "FooterButtonActive--Ravenclaw" : ""
+        }
         onClick={() => buttonStateHandler("Ravenclaw")}
       />
       <NavLink
         src={sIcon}
         ariaLabel="Slytherin"
-        className={buttonState === "Slytherin" ? "FooterButtonActive" : ""}
+        className={
+          buttonState === "Slytherin" ? "FooterButtonActive--Slytherin" : ""
+        }
         onClick={() => buttonStateHandler("Slytherin")}
       />
     </footer>
